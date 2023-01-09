48718554d114277e85337bebe80f5051ba7972a8
Alvinjasper <alvinjspr@gmail.com>
2023-01-09 06:00:59 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -1,145 +1,45 @@
-#include"main.h"
-
+#include "main.h"
 /**
- * spec_eng - Calls the correct print function and returns a string
- *
- * @list: the va_list object
- * @data: the parsed complete specifier
- * @buffer: the main buffer
- *
- * Return: a string
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
  */
-int spec_eng(va_list list, spec_data_t *data, pf_buf_t *buffer)
+int _printf(const char * const format, ...)
 {
-	pf_buf_t *tmp;
-	int len = 0, i, j = 0;
-	specs_t specs[] = {
-		{'c', store_char},
-		{'s', store_string},
-		{'d', store_int},
-		{'i', store_int},
-		{'u', store_uint},
-		{'o', store_uoct},
-		{'x', store_lowuhex},
-		{'X', store_upuhex},
-		{'r', store_rev},
-		{'R', store_rot13},
-		{'b', store_binary},
-		{'S', store_strnop},
-		{'p', store_ptr},
-		{0, NULL}
+	convert_match m[] = {
+		{"%s", printf_string}, {"%c", printf_char},
+		{"%%", printf_37},
+		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
+		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
+		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
+		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
 	};
 
-	for (i = 0; specs[i].spec; i++)
-	{
-		if (data->fmt_spec == specs[i].spec)
-		{
-			tmp = specs[i].func(list, data);
-			if (tmp)
-			{
-				while (j < tmp->len)
-					pf_buf_t_add_char(buffer, tmp->buf[j++]);
-				len = tmp->len;
-				pf_buf_t_delete(tmp);
-			}
-		}
-	}
-	return (len);
-}
+	va_list args;
+	int i = 0, j, len = 0;
 
-/**
- * format_parsing - the function that do the parsing on the format and
- * executes the spec_eng function
- *
- * @i: the format index
- * @format: the format string
- * @buf: the main buffer
- * @list: the list of parameters
- *
- * Return: the size of the returned element
- */
-int format_parsing(int *i, const char *format, pf_buf_t *buf, va_list list)
-{
-	spec_data_t *data = NULL;
-	int status = OK;
-	int ret_value = -1;
-
-	data = spec_data_t_new();
-	if (data)
-	{
-		status = spec_data_t_parse(data, format + (*i));
-		if (status == OK)
-		{
-			if (data->fmt_spec == '%')
-			{
-				pf_buf_t_add_char(buf, '%');
-				(*i)++;
-				ret_value = 1;
-			}
-			else
-			{
-				(*i) += data->fmt_len;
-				ret_value = spec_eng(list, data, buf);
-			}
-		}
-		else if (status == INVALID)
-		{
-			pf_buf_t_add_char(buf, '%');
-			ret_value = 1;
-		}
-		else
-		{
-			ret_value = -1;
-		}
-		spec_data_t_delete(data);
-	}
-	return (ret_value);
-}
-
-/**
- * _printf - entry point for our main function
- *
- * @format: the format string
- *
- * Return: the number of characters printed (excluding the null byte)
- */
-int _printf(const char *format, ...)
-{
-	va_list list;
-	int i = 0, len = 0;
-	pf_buf_t *buffer;
-	int total_len = 0, tmp_len = 0;
-
-	buffer = pf_buf_t_new(BUFSIZE);
-	if (!buffer)
-		return (0);
-
-	va_start(list, format);
-
-	if (!format)
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
-	len = _strlen((char *)format);
-	while (format[i] && i < len)
+Here:
+	while (format[i] != '\0')
 	{
-		while (format[i] && format[i] != '%')
+		j = 13;
+		while (j >= 0)
 		{
-			pf_buf_t_add_char(buffer, format[i++]);
-			total_len++;
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			{
+				len += m[j].f(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
 		}
-
-		if (format[i] == '%' && len > 1)
-		{
-			tmp_len = format_parsing(&i, format, buffer, list);
-			if (tmp_len == -1)
-				return (-1);
-			total_len += tmp_len;
-			tmp_len = 0;
-		}
+		_putchar(format[i]);
+		len++;
 		i++;
 	}
-	pf_buf_t_print(buffer);
-	va_end(list);
-	pf_buf_t_delete(buffer);
-	return (total_len);
+	va_end(args);
+	return (len);
 }
